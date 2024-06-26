10d28201e0265ccd8ac1ad77834fe2c2699c0b36
PenLoo98 <sohnchanho@gmail.com>
2024-06-26 14:15:08 +0900

    Initial commit: Create rtc-chatting
    Create rtc-chatting Spring Boot project

---
+++ rtc-chatting/src/main/java/com/myweapon/rtc_chatting/RtcChattingApplication.java
@@ -0,0 +1,13 @@
+package com.myweapon.rtc_chatting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtcChattingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtcChattingApplication.class, args);
+	}
+
+}
---
+++ rtc-chatting/src/test/java/com/myweapon/rtc_chatting/RtcChattingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myweapon.rtc_chatting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtcChattingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
